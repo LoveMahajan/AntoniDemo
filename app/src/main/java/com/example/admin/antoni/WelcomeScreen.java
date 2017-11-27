2168ce3a8a4edba3eb0cce1eb609d04044d0d0aa
LoveMahajan <lovemahajan@techugo.com>
2017-11-27 16:59:11 +0530

    New

--- app/src/main/java/com/example/admin/antoni/WelcomeScreen.java
+++ app/src/main/java/com/example/admin/antoni/WelcomeScreen.java
@@ -1,13 +1,33 @@
 package com.example.admin.antoni;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeScreen extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen);
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WelcomeScreen.this,Login.class));
+            }
+        });
+
+        findViewById(R.id.btnFb).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WelcomeScreen.this,AddInfo.class));
+            }
+        });
     }
 }
