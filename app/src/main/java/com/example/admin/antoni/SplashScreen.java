5b2d3a3642d88fe3732981d65af5eb89a1add504
LoveMahajan <lovemahajan@techugo.com>
2017-12-05 10:28:10 +0530

    New

--- app/src/main/java/com/example/admin/antoni/SplashScreen.java
+++ app/src/main/java/com/example/admin/antoni/SplashScreen.java
@@ -1,22 +1,51 @@
 package com.example.admin.antoni;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+
+    Boolean isLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        isLogin=false;
+
+        startNewActivity();
+    }
+
+    public void startNewActivity()
+    {
         new Handler().postDelayed(new Runnable() {
 
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this,WelcomeScreen.class));
-                finish();
+                SharedPreferences preferences=getSharedPreferences("mypreference", Context.MODE_PRIVATE);
+                if(preferences.contains("isLogin"))
+                {
+                    isLogin=preferences.getBoolean("isLogin",false);
+
+                }
+
+                if(isLogin==true)
+                {
+                    Utility.startActivity(SplashScreen.this,Home.class);
+                    finish();
+                }
+                else
+                {
+                    startActivity(new Intent(SplashScreen.this, WelcomeScreen.class));
+                    finish();
+                }
+
+
             }
 
         }, 3000);
--- app/src/main/java/com/example/admin/antoni/WelcomeScreen.java
+++ app/src/main/java/com/example/admin/antoni/WelcomeScreen.java
@@ -1,33 +1,62 @@
 package com.example.admin.antoni;
 
+import android.content.Context;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class WelcomeScreen extends AppCompatActivity {
+import com.example.admin.antoni.databinding.WelcomeScreenBinding;
 
 
+public class WelcomeScreen extends AppCompatActivity implements View.OnClickListener{
 
+    ConstraintLayout btnFB,btnGoogle,btnEmail;
+    WelcomeScreenBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen);
 
-        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(WelcomeScreen.this,Login.class));
-            }
-        });
+        binding= DataBindingUtil.setContentView(WelcomeScreen.this,R.layout.welcome_screen);
 
-        findViewById(R.id.btnFb).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(WelcomeScreen.this,AddInfo.class));
-            }
-        });
+        binding.btnFb.setOnClickListener(this);
+        binding.btnGoogle.setOnClickListener(this);
+        binding.btnEmail.setOnClickListener(this);
+        binding.login.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view==binding.btnFb)
+        {
+            Intent intent=new Intent(WelcomeScreen.this,AddInfo.class);
+
+            intent.putExtra("Login","facebook");
+            startActivity(intent);
+            finish();
+        }
+        if(view==binding.btnGoogle)
+        {
+            Intent intent=new Intent(WelcomeScreen.this,AddInfo.class);
+
+            intent.putExtra("Login","google");
+            startActivity(intent);
+            finish();
+        }
+        if(view==binding.btnEmail)
+        {
+            startActivity(new Intent(WelcomeScreen.this,Signup.class));
+        }
+        if(view==binding.login)
+        {
+            startActivity(new Intent(WelcomeScreen.this,Login.class));
+        }
     }
 }
